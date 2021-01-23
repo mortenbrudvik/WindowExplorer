980ced6010b7bf97ab2b7c617d784ef90181b248
Morten Brudvik <morten@brudvik.com>
2021-01-23 18:37:42 +0100

    Added console project, when run will show all available application windows

--- src/WindowExplorer/Program.cs
+++ src/WindowExplorer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using ApplicationCore.Extensions;
+using ApplicationCore.Window;
+using Infrastructure;
 
 namespace WindowExplorer
 {
@@ -6,7 +10,24 @@ namespace WindowExplorer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var windowFactory = new WindowFactory();
+                var windows = windowFactory.GetWindows(WindowFilter.NormalWindow);
+
+                var options = new LoggerTableOptions { Columns = new List<string> {"Handle", "Class Name", "Title", "Process Name", "ProcessId"}};
+                var tableLogger = new TableLogger(options);
+                windows.ForEach(win =>
+                {
+                    tableLogger.AddRow(win.Handle, win.ClassName.Truncate(50, ""), win.Title.Truncate(50, ""), win.ProcessName.Truncate(30, ""), win.ProcessId);
+                });
+                tableLogger.Write(Format.Minimal);
+                Console.Out.WriteLine($"Windows found: {windows.Count}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred when listing the windows. Error message: " + e.Message);
+            }            
         }
     }
 }
